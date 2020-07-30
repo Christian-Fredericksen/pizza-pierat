46b1f570899a53057ba36b391dd6366856577120
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-07-29 17:08:29 -0700

    generate customer resourse

---
+++ db/migrate/20200729235656_create_customers.rb
@@ -0,0 +1,15 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
