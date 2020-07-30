dc510f51b06e0b3c23409dce30baec0c67d2ed1f
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-07-30 11:45:16 -0700

    add form_for customer and route to show page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,4 +3,20 @@ class CustomersController < ApplicationController
     def new
         @customer = Customer.new
     end
+
+    def create
+        @customer = Customer.new(customer_params)
+        @customer.save
+        redirect_to customer_path(@customer)
+    end     
+
+    def show
+        @customer = Customer.find(params[:id])
+    end
+
+    private
+    def customer_params
+        params.require(:customer).permit(:first_name, :last_name, :street_address, :city, :state, 
+        :zip_code,:email, :phone_number)
+    end
 end
